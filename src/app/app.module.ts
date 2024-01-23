c4689345cb7f18ce0db8543bb58a62d019267c62
Wesley Emanuel <wesleyemanuel.1517@gmail.com>
2024-01-23 13:16:49 -0300

    feat: creates card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { CadsListComponent } from './components/cads-list/cads-list.component';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { CommonModule } from '@angular/common';
     HeaderComponent,
     FormsComponent,
     CadsListComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
